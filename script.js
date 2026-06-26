e0ee87dc2dc41fd403fb245ef38b8b7b498e41d4
Mehedi Hossen Fahim <mehdihossenfahim100@gmail.com>
2026-06-26 14:38:21 +0600

    Add Task Complete.

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
+
+let taskList = [
+    {
+        "name": "Learn JavaScript",
+        "subject": "Web Development",
+        "priority": "high",
+        "completed": false
+    }
+]
 const year = new Date().getFullYear();
-const date = new Date().getDate();
-const month = new Date().getMonth();
+
+// console.log(taskList);
+// taskList.forEach((task, idx) => {
+//     console.log(idx);
+//     console.log(task.name);
+//     console.log(task.subject);
+//     console.log(task.priority);
+//     console.log(task.status);
+
+// });
 
 
 
-document.querySelector("#date").textContent = `${date} ${month} ${year}`;
+// ----------------------------------
+//-------- Add Task to the list -----
+// ----------------------------------
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const data = new FormData(this);
+    // values from html
+    // console.log(data.get("title"), data.get("subject"), data.get("priority"));
+    taskList.push({
+        "name": data.get("title"),
+        "subject": data.get("subject"),
+        "priority": data.get("priority"),
+        "status": false
+    });
+
+
+    console.log(taskList);
+    taskList.forEach((task, idx) => {
+        console.log(`Task ${idx + 1}:`);
+        console.log(task.name);
+        console.log(task.subject);
+        console.log(task.priority);
+        console.log(task.completed);
+
+    });
+});
+
+
+
 
 document.querySelector("#footerYear").textContent = year;
